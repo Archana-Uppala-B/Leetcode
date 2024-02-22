8b018228098d52a1792727e47e21510f47ae0625
Archana Uppala B <80192022+Archana-Uppala-B@users.noreply.github.com>
2024-02-22 17:26:57 +0530

    Time: 2 ms (99.85%), Space: 54 MB (27.14%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        //indegree-outdegree = 1;
+        
+        int[] count=new int[n+1];
+        
+        for(int[] tr: trust)
+        {
+            count[tr[0]]--;
+            count[tr[1]]++;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(count[i]==n-1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
