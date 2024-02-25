48e90f1e4b563b73749af82b800c15abcb8b75b6
Archana Uppala B <80192022+Archana-Uppala-B@users.noreply.github.com>
2024-02-25 10:37:57 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (87.42%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String smaller = (str1.length()>str2.length())? str2 : str1;
+        String bigger = (str1.length()>str2.length()) ? str1 : str2;
+        
+        if(bigger.equals(smaller)) 
+            return smaller;
+        
+        if(!bigger.startsWith(smaller)) 
+             return "";
+        
+        return gcdOfStrings(bigger.substring(smaller.length()),smaller);
+    }
+}
